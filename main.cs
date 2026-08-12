68add9a9f1ab32725ea230a54e6805a8a302310e
Lauren <brenderlauren4@gmail.com>
2026-08-12 16:02:31 +0100

    add initial main to file structuire

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace mainTeaRex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, TeaRex!");
+        }
+    }
+}
